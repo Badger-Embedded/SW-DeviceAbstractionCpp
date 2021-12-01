1a17ed73c72aef9f80aa45c74f3fb43bd8b749d3
Talha Can Havadar <havadartalha@gmail.com>
2021-12-01 21:28:57 +0100

    Update device class

--- include/device.h
+++ include/device.h
@@ -1,12 +1,21 @@
 #if !defined(BADGER_DEVICE_H)
 #define BADGER_DEVICE_H
+#include <string>
 
 namespace Badger
 {
+    enum class DeviceType {
+        HUB,
+        NODE
+    };
+
     class Device {
     public:
         Device();
         virtual ~Device();
+    private:
+        DeviceType m_type;
+        std::string m_name;
     };
 } // namespace Badger
 
